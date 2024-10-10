69f4a8c6394d38779a421cba8f80171314f68441
Tristan Luong <tristan@qlower.com>
2024-10-10 11:50:40 +0200

    Fix component

--- src/components/swagger-doc.tsx
+++ src/components/swagger-doc.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
-const SwaggerDoc = () => (
-  <SwaggerUI url="https://dev.api.qlower.com/swagger.json" />
-);
+const SwaggerDoc = () => {
+  const isClient = typeof window !== "undefined";
+  if (!isClient) {
+    return null;
+  }
+
+  return <SwaggerUI url="https://dev.api.qlower.com/swagger.json" />;
+};
 
 export default SwaggerDoc;
